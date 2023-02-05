627c333ff77f9f964e25550db45948f35cd03f7c
BrooklynItalian <michaelpetrillo2001@gmail.com>
2023-02-05 16:13:15 -0500

    0

---
+++ Michael_Petrillo_nodejs-express-mysql - Copy/app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "database.ccmd6j6rmnku.us-east-1.rds.amazonaws.com",
+  USER: "admin",
+  PASSWORD: "password",
+  DB: "CS164_Michael Petrillo"
+};
---
+++ Michael_Petrillo_nodejs-express-mysql - Copy/app/models/user.model.js
@@ -0,0 +1,129 @@
+const sql = require("./db.js");
+
+// constructor
+const Users = function(users) {
+  this.iduserID = users.iduserID;
+  this.user = users.user;
+  this.password= users.password;
+};
+
+Users.create = (newUser, result) => {
+  sql.query("INSERT INTO userID SET ?", newUser, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created User: ", { id: res.insertId, ...newUser });
+    result(null, { id: res.insertId, ...newUser });
+  });
+};
+
+Users.findById = (iduserID, result) => {
+  sql.query(`SELECT * FROM userID WHERE id = ${iduserID}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found User: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found User with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+Users.getAll = (iduserID, result) => {
+  let query = "SELECT * FROM Users";
+
+  if (iduserID) {
+    query += ` WHERE iduserID LIKE '%${iduserID}%'`;
+  }
+
+  sql.query(query, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("Users: ", res);
+    result(null, res);
+  });
+};
+
+Users.getAllpassword= result => {
+  sql.query("SELECT * FROM userID WHERE published=true", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("Users: ", res);
+    result(null, res);
+  });
+};
+
+Users.updateById = (id, User, result) => {
+  sql.query(
+    "UPDATE Users SET iduserID = ?, user = ?, password= ? WHERE id = ?",
+    [User.iduserID, User.user, User.published, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found User with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated User: ", { id: id, ...User });
+      result(null, { id: id, ...User });
+    }
+  );
+};
+
+Users.remove = (id, result) => {
+  sql.query("DELETE FROM Users WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found User with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted User with id: ", id);
+    result(null, res);
+  });
+};
+
+Users.removeAll = result => {
+  sql.query("DELETE FROM Users", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`deleted ${res.affectedRows} Users`);
+    result(null, res);
+  });
+};
+
+module.exports = Users;
